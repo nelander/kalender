5362ff4848876e3e09be309e4a24c51239ad2339
Bengt Nelander <bengt@nelander.de>
2016-02-24 13:25:53 +0100

    Weitere Dateien

---
+++ Zugangsdaten.php
@@ -0,0 +1,9 @@
+<?php
+//
+// Konstanten für den MySQL-Server und die Zugangsdaten  TEST-SERVER
+//
+$db_server   = "localhost";			// Web-Server
+$db_benutzer = "U2447799";			// MySQL User
+$db_passwort = "hemligaordet123";	// MySQL Password
+$db_name     = "DB2447799";			// MySQL Datenbank
+?>
